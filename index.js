47982ca34fb3c6ffce08c10d5e8c37e59fe45765
Albert Portnoy <albert@albertp.dev>
2022-07-21 12:43:01 -0500

    Initial commit 2

---
+++ index.js
@@ -0,0 +1,63 @@
+const { Plugin } = require('powercord/entities');
+const { inject, uninject } = require('powercord/injector');
+const { getModule } = require('powercord/webpack');
+
+const SPACER_CHARACTER_SET = [
+	'\\s',
+	'‏',
+	'‎',
+	'ㅤ',
+	'⠀',
+	'━',
+	'╶',
+	'╴',
+	'─',
+	'-',
+];
+
+const REGEX = new RegExp(`^[${SPACER_CHARACTER_SET.join('')}]{5,}(.+?)[${SPACER_CHARACTER_SET.join('')}]{5,}$`);
+
+module.exports = class MessageTooltips extends Plugin {
+	async startPlugin() {
+		this.MemberRole = getModule(['MemberRole'], false);
+		this.Heading = getModule(['Heading'], false).Heading;
+		this.bodyTitle = getModule(['bodyTitle'], false).bodyTitle;
+
+		this.injectRoles();
+
+		console.log(this.MemberRole);
+	}
+
+	injectRoles() {
+		inject('roledividers', this.MemberRole, 'default', (_, res) => {
+			const rendered = res.type(res.props);
+			const rendered2 = rendered.props.children.type(rendered.props.children.props);
+			const roles = rendered2.props.children[0];
+
+			roles.forEach((data, i) => {
+				const role = data.props.role;
+				const name = role.name;
+				const match = name.match(REGEX);
+				if (match) roles[i] = this.Heading({
+					variant: 'eyebrow',
+					className: this.bodyTitle,
+					color: 'header-secondary',
+					level: 3,
+					children: match[1],
+					style: {
+						width: '100%',
+						marginTop: '8px',
+					},
+				});
+			});
+
+			res.type = () => rendered;
+
+			return res;
+		});
+	}
+
+	pluginWillUnload() {
+		uninject('roledividers');
+	}
+};
